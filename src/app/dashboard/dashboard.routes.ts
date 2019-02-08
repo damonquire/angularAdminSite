ff85d068e9387a411290bf9e54532467b6a577e6
Damon T Quire <dtquir01@louisville.edu>
2019-02-08 10:03:08 -0500

    changes

--- src/app/dashboard/dashboard.routes.ts
+++ src/app/dashboard/dashboard.routes.ts
@@ -1,19 +1,18 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
-import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
 import { AuthGuard } from '../guards/auth-guard.service';
 import { RoleGuard } from '../guards/role-guard.service';
 import { SolutionsComponent } from '../solutions/solutions.component';
-
+import {LoginComponent} from '../login/login.component';
 export const dashboardRoutes: Routes = [
   {
     path: 'dashboard',
     component: LayoutComponent,
     canActivate: [AuthGuard],
     children: [
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent},
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent},
       {
         path: 'admin', component: AdminComponent,
         data: {role: 'Admin'},
